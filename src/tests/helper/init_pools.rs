00c4199e70df435cb178a8cd76fa6bb948a9aa93
ksyao <ksyao2002@gmail.com>
2025-02-16 23:06:39 -0600

    finally owned token is fixed

--- src/tests/helper/init_pools.rs
+++ src/tests/helper/init_pools.rs
@@ -50,7 +50,7 @@ pub fn init_block_with_amm_pool() -> Result<(Block, AmmTestDeploymentIds)> {
             target: AlkaneId { block: 1, tx: 0 },
             inputs: vec![0, AMM_FACTORY_ID],
         },
-        // token 1 init 1 auth token and mint 1000000 owned tokens
+        // token 1 init 1 auth token and mint 1000000 owned tokens. Also deploys owned token contract at {2,2}
         Cellpack {
             target: AlkaneId { block: 1, tx: 0 },
             inputs: vec![0, 1, 1000000],
@@ -65,15 +65,11 @@ pub fn init_block_with_amm_pool() -> Result<(Block, AmmTestDeploymentIds)> {
             target: AlkaneId { block: 5, tx: 2 }, // factory creation of owned token using {2, 2} as the factory
             inputs: vec![0, 1, 1000000],
         },
-        // oyl token init 1 auth token and mint 1000000 owned tokens. TODO: this will cause indexer to break, not sure why yet. Stack trace below
-        /*
-            panicked at /Users/kevinyao/.cargo/git/checkouts/alkanes-rs-6aa9d88f67afd990/b4f5f21/src/vm/utils.rs:26:14:
-            range end index 2228716 out of range for slice of length 1179648
-        */
-        // Cellpack {
-        //     target: AlkaneId { block: 5, tx: 2 }, // factory creation of owned token using {2, 2} as the factory
-        //     inputs: vec![0, 1, 1000000],
-        // },
+        // oyl token init 1 auth token and mint 1000000 owned tokens.
+        Cellpack {
+            target: AlkaneId { block: 5, tx: 2 }, // factory creation of owned token using {2, 2} as the factory
+            inputs: vec![0, 1, 1000000],
+        },
         // router
         Cellpack {
             target: AlkaneId { block: 1, tx: 0 },
@@ -95,7 +91,7 @@ pub fn init_block_with_amm_pool() -> Result<(Block, AmmTestDeploymentIds)> {
             alkanes_std_owned_token_build::get_bytes(),
             [].into(),
             [].into(),
-            // [].into(),
+            [].into(),
             router_build::get_bytes(),
             oyl_factory_build::get_bytes(),
         ]
@@ -145,14 +141,14 @@ pub fn init_block_with_amm_pool() -> Result<(Block, AmmTestDeploymentIds)> {
             block: 2,
             tx: tx_iterator.next().unwrap(),
         },
-        // oyl_token_deployment: AlkaneId {
-        //     block: 2,
-        //     tx: tx_iterator.next().unwrap(),
-        // },
-        // oyl_auth_token_deployment: AlkaneId {
-        //     block: 2,
-        //     tx: tx_iterator.next().unwrap(),
-        // },
+        oyl_token_deployment: AlkaneId {
+            block: 2,
+            tx: tx_iterator.next().unwrap(),
+        },
+        oyl_auth_token_deployment: AlkaneId {
+            block: 2,
+            tx: tx_iterator.next().unwrap(),
+        },
         amm_router_deployment: AlkaneId {
             block: 2,
             tx: tx_iterator.next().unwrap(),
@@ -212,10 +208,10 @@ pub fn assert_contracts_correct_ids(
         deployment_ids.owned_token_3_deployment.clone(),
         alkanes_std_owned_token_build::get_bytes(),
     );
-    // let _ = assert_binary_deployed_to_id(
-    //     deployment_ids.oyl_token_deployment.clone(),
-    //     alkanes_std_owned_token_build::get_bytes(),
-    // );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.oyl_token_deployment.clone(),
+        alkanes_std_owned_token_build::get_bytes(),
+    );
     let _ = assert_binary_deployed_to_id(
         deployment_ids.auth_token_1_deployment.clone(),
         alkanes_std_auth_token_build::get_bytes(),
@@ -228,10 +224,10 @@ pub fn assert_contracts_correct_ids(
         deployment_ids.auth_token_3_deployment.clone(),
         alkanes_std_auth_token_build::get_bytes(),
     );
-    // let _ = assert_binary_deployed_to_id(
-    //     deployment_ids.oyl_auth_token_deployment.clone(),
-    //     alkanes_std_auth_token_build::get_bytes(),
-    // );
+    let _ = assert_binary_deployed_to_id(
+        deployment_ids.oyl_auth_token_deployment.clone(),
+        alkanes_std_auth_token_build::get_bytes(),
+    );
     let _ = assert_binary_deployed_to_id(
         deployment_ids.amm_pool_1_deployment.clone(),
         pool_binary.clone(),
@@ -308,8 +304,8 @@ pub fn insert_init_oyl_pool_liquidity_txs(
                 target: deployment_ids.oyl_amm_factory_deployment,
                 inputs: vec![
                     1,
-                    // deployment_ids.oyl_token_deployment.block, // TODO: change this to oyl token
-                    // deployment_ids.oyl_token_deployment.tx,
+                    deployment_ids.oyl_token_deployment.block,
+                    deployment_ids.oyl_token_deployment.tx,
                 ],
             }],
             OutPoint {
